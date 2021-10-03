4d55916e2fc45072f1163da244cbbff712ad50ac
wbrunovieira <wbrunovieira77@gmail.com>
2021-10-03 22:49:23 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const { request, response } = require('express');
+const express =  require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333);
+
+app.get('/courses', (request, response) => {
+    const query = request.query
+    console.log(query)
+    response.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3"
+    ])
+})
+
+
+app.post('/courses', (request, response) => {
+    
+    const body = request.body
+    console.log(body);
+    response.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3",
+        "Curso 4"
+    ])
+})
+
+app.put('/courses/:id', (request, response) => {
+
+    const { id } = request.params;
+    console.log(id)
+
+    response.json([
+        "Curso 6",
+        "Curso 2",
+        "Curso 3",
+        "Curso 4"
+    ])
+})
+
+app.patch('/courses/:id', (request, response) => {
+    response.json([
+        "Curso 6",
+        "Curso 7",
+        "Curso 3",
+        "Curso 4"
+    ])
+})
+
+app.delete('/courses/:id', (request, response) => {
+    response.json([
+        "Curso 6",
+        "Curso 7",
+        "Curso 3",
+        
+    ])
+})
